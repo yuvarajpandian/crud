af33376d2c42ebfeac7a5a2f104626dbdc647f86
yuvarajpandian <yuvarajpandian50@gmail.com>
2023-01-24 20:58:21 +0530

    db connection

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-const express = require('express');
-const mongoose = require('mongoose');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
+
+dotenv.config({ path: "./src/config/databaseConfig.env" });
+
+app.use(express.json({ limit: "50kb" }));
+
+mongoose
+  .connect(process.env.DB_URI, { useNewUrlParser: true })
+  .then((res) => {
+    console.log("DataBase successfully connected");
+  })
+  .catch((err) => {
+    console.log("database is not connected", err);
+  });
+
+app.listen(process.env.PORT, () => {
+  console.log("listening to the port", process.env.PORT);
+});
